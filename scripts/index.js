04b4b902d6de473a9860071d5206308c03099691
mcatsecurity <a.manrique742@gmail.com>
2022-08-24 20:03:09 -0500

    commit 1

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+var first = document.getElementById("first")
+var second = document.getElementById("second"); 
+
+function showSecond() {
+    second.style.display = "block"
+    first.style.display = "none"
+}
