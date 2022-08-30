9b06ecf1104b5bdd6e7358d874b8bd00339eeaf7
mcatsecurity <a.manrique742@gmail.com>
2022-08-30 10:21:15 -0500

    commit2

---
+++ scripts/ejemplo.js
@@ -0,0 +1,7 @@
+function warning() {
+    document.getElementById("title").innerHTML = "MCATSECURITY"
+    alert("¡Este sitio web es peligroso!")
+    window.location.href="file:///C:/Users/amanr/1stpractica/proyectos/web/cole/ipFabio/warning.html"
+}
+
+setTimeout(warning, 5000)
